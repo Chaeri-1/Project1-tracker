3b861264565e47a744d2335969079935f642c976
Chaeri-1 <alfsdo03@husf.ac.kr>
2024-07-18 12:54:38 +0900

    project setting

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import ErrorPage from './errorPage'
+import Root from './routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    childern: [
+      {
+        index: true,
+        element:<p>Start</p>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
